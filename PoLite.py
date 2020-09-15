Added independent masking procedures. Fixed bug in Bmap method where the data would be overwritten if called multiple times.

PoLite.py
@@ -9,7 +9,7 @@ submillimeter polarimetric data sets.
 
 The two instruments currently supported are HAWC+ on the Stratospheric 
 Observatory for Infrared Astronomy (SOFIA) and POL-2 at the James Clerk Maxwell
-Telescope (JCMT). This script will someday include ALMA, APEX, NIKA-2, Planck.
+Telescope (JCMT). This script may someday include ALMA, APEX, NIKA-2, Planck.
 
 Acknowledgements:
     This research made use of APLpy, an open-source plotting package for Python
@@ -31,12 +31,41 @@ from astropy.io import fits
 from astropy import wcs
 from aplpy import FITSFigure
 import math
+#import warnings
+
+# Warning filter
+#warnings.simplefilter('ignore',category=wcs.FITSFixedWarning)
+# The following mutes warnings about NaN values
+np.warnings.filterwarnings('ignore')
+
+# =============================================================================
+# Object containing the polarization data and important ancillary information
+# =============================================================================
+class cat:
+    # Default attributes of the object
+    def __init__(self):
+        # Polarization data
+        self.I = np.empty([2]) # Stokes I
+        self.dI = np.empty([2]) # Uncertainty dI for Stokes I
+        self.Q = np.empty([2]) # Stokes Q
+        self.dQ = np.empty([2]) # Uncertainty dQ for Stokes Q
+        self.U = np.empty([2]) # Stokes U
+        self.dU = np.empty([2]) # Uncertainty dU for Stokes U
+        self.PI = np.empty([2]) # Polarized intensity PI (debiased)
+        self.dPI = np.empty([2]) # Uncertainty dPI for polarized intensity PI
+        self.P = np.empty([2]) # Polarization fraction P (debiased)
+        self.dP = np.empty([2]) # Uncertainty dP for polarization fraction P
+        self.O = np.empty([2]) # Polarization angle O
+        self.B = np.empty([2]) # Rotated polarization angle B (+90 degrees)
+        self.dO = np.empty([2]) # Uncertainty dO for polarization angle O
+        self.RA = np.empty([2]) # Right Ascension coordinates in degrees
+        self.DEC = np.empty([2]) # Declination coordinates in degrees
 
 # =============================================================================
 # Object containing the polarization data and important ancillary information
 # =============================================================================
 class obs:
-    # Initial attributes of the GREAT array
+    # Default attributes of the object
     def __init__(self):
         # Polarization data
         self.I = np.empty([2,2]) # Stokes I
@@ -52,6 +81,8 @@ class obs:
         self.O = np.empty([2,2]) # Polarization angle O
         self.B = np.empty([2,2]) # Rotated polarization angle B (+90 degrees)
         self.dO = np.empty([2,2]) # Uncertainty dO for polarization angle O
+        self.RA = np.empty([2,2]) # Right Ascension coordinates in degrees
+        self.DEC = np.empty([2,2]) # Declination coordinates in degrees
         
         # Observation properties
         self.instrument = 'None' # Instrument used for observations
@@ -67,7 +98,7 @@ class obs:
 # =============================================================================
 # Plotting procedure for polarization maps
 # =============================================================================
-    def polmap(self, idi=50.0, pdp=3.0, pmax=40.0, color='rainbow', 
+    def polmap(self, idi=50.0, pdp=3.0, pmax=30.0, color='rainbow', 
                scalevec=0.4, clevels=100, imin=0.0, imax=None, 
                size_x = 9.0, size_y = 9.0, dpi = 100.0):
         # Initialization
@@ -108,7 +139,7 @@ class obs:
         polmap.show_colorscale(cmap=color, vmin=imin, vmax=imax)
         # Plotting a filled contour plot of the data
         polmap.show_contour(cmap=color, levels=clevels, filled=True, 
-                            extend='both')
+                            extend='both', vmin=imin, vmax=imax)
         
         # Adding a colorbar to the plot
         polmap.add_colorbar(pad=0.125)
@@ -119,9 +150,15 @@ class obs:
         
         # Creating a new fits object for APLpy's FITSFigure method to recognize
         # for the polarization fraction P
-        pmap = fits.PrimaryHDU(data=self.P, header=self.header)
-        # for the polarization angle O
-        omap = fits.PrimaryHDU(data=self.O, header=self.header)
+        pref = fits.PrimaryHDU(data=self.P, header=self.header)
+        pmap = pref.copy()
+        # for the magnetic field angle B
+        oref = fits.PrimaryHDU(data=self.O, header=self.header)
+        omap = oref.copy()
+        # A copy of the Primary HDU is created before any masking is done.
+        # This fixes an issue where modifying pmap was carried to other
+        # instances of this method. Future fix should make sure pref is
+        # closed/deleted at the end of this method.
         
         # Muting the numpy alerts triggered by nan values
         np.warnings.filterwarnings('ignore')
@@ -162,7 +199,7 @@ class obs:
               ' improve it (see online APLpy.FITSFigure documentation)')
         print()
         print('Don\'t forget to save the results using the' + 
-              ' .save(\'name.png\') function')
+              ' .savefig(\'name.png\') function')
         print()
         print('Don\'t give up!')
         
@@ -200,7 +237,8 @@ class obs:
         # for the Stokes I total intensity
         plot_hdu = fits.PrimaryHDU(data=self.I, header=self.header)
         # Loading the data in an APLpy figure 
-        Bmap = FITSFigure(plot_hdu, dpi=dpi, figsize=(size_x, size_y))
+        Bmap = FITSFigure(plot_hdu, dpi=dpi, figsize=(size_x, size_y),
+                          slices=[0,1])
         
         print('Plotting the Stokes I total intensity map')
         print()
@@ -219,13 +257,19 @@ class obs:
         Bmap.colorbar.set_axis_label_text(self.units)
         # Moving the colorbar to be on top of the figure
         Bmap.colorbar.set_location('top')
-        
+              
         # Creating a new fits object for APLpy's FITSFigure method to recognize
         # for the polarization fraction P
-        pmap = fits.PrimaryHDU(data=self.P, header=self.header)
-        # for the polarization angle O
-        omap = fits.PrimaryHDU(data=self.B, header=self.header)
-        
+        pref = fits.PrimaryHDU(data=self.P, header=self.header)
+        pmap = pref.copy()
+        # for the magnetic field angle B
+        oref = fits.PrimaryHDU(data=self.B, header=self.header)
+        omap = oref.copy()
+        # A copy of the HDU is created before any masking is done.
+        # This fixes an issue where modifying pmap was carried to other
+        # instances of this method. Future fix should make sure pref is
+        # closed/deleted at the end of this method.
+                
         # Muting the numpy alerts triggered by nan values
         np.warnings.filterwarnings('ignore')
         
@@ -265,11 +309,134 @@ class obs:
               ' improve it (see online APLpy.FITSFigure documentation)')
         print()
         print('Don\'t forget to save the results using the' + 
-              ' .save(\'name.png\') function')
+              ' .savefig(\'name.png\') function')
         print()
         print('Have fun!')
         
         return Bmap
+
+# =============================================================================
+# Creating a mask for a region of the map
+# =============================================================================
+    def MaskRegion(self, shape=None, center_RA=None, center_DEC=None, 
+                   radius=None, width=None, height=None):
+        # Method to create a simple mask (either circular or rectangular) for 
+        # data analysis.
+        
+        # Initialization
+        # shape : Shape of the region masked, either "Circle" or "Rectangle".
+        # center_RA : Central position of the mask in right ascension (degrees)
+        # center_DEC : Central position of the mask in declination (degrees)
+        # radius: If shape is Circle, then this paramter provides its radius (degrees)
+        # width: If shape is Rectangle, then this paramter provides its width in right ascencion (degrees)
+        # height: If shape is Circle, then this paramter provides its heigh in declination (degrees)
+        
+        print()
+        print('=============================')
+        print('Respectfully masking a region')
+        print('=============================')
+        print()
+        
+        # Creating the empty array for the final mask
+        mask = fits.PrimaryHDU(data=np.zeros(self.size), header=self.header)
+        
+        # Checking the shape for the region
+        if shape == 'Circle':
+            print('Creating a circular mask')
+            print()
+        elif shape == 'Rectangle':
+            print('Creating a rectangular mask')
+            print()
+        else:
+            print('Shape should be "Circle" or "Rectangle"')
+            return mask
+        
+        # Checking the coordinates of the central posituon
+        if center_RA == None or center_DEC == None:
+            print('Please provide the center_RA and center_DEC parameters '+
+                  'of the central position in degrees')
+            return mask
+        
+        # Checking the parameters for the plotted shape
+        if shape == 'Circle' and radius == None:
+            print('Please provide the radius of the circle in degrees')
+            return mask
+        if shape == 'Rectangle' and (width == None or height == None):
+            print('Please provide the length and height of the rectangle '+
+                  'in degrees')
+            return mask
+        
+        # If the shape is a Circle
+        if shape == 'Circle':            
+            # Identifying pixels to be included in the mask
+            for i in range (0,self.size[0]): # Declination
+                for j in range (0,self.size[1]): # Right Ascension
+                    # Distance to central pixel
+                    delta = (((self.RA[i,j] - 
+                               center_RA)*math.cos(self.DEC[i,j]*math.pi/180.0
+                                                   ))**2.0 + 
+                              (self.DEC[i,j] - center_DEC)**2.0)**0.5
+                    # Is the pixel in the mask?
+                    if delta < radius:
+                        mask.data[i,j] = 1.0
+                    else:
+                        mask.data[i,j] = np.nan
+        
+        # If the shape is a Rectangle
+        if shape == 'Rectangle':            
+            # Identifying pixels to be included in the mask
+            for i in range (0,self.size[0]): # Declination
+                for j in range (0,self.size[1]): # Right Ascension
+                    # Distance to central pixel
+                    delta_ra = abs((self.RA[i,j] - 
+                               center_RA) * math.cos(
+                                   self.DEC[i,j]*math.pi/180.0))
+                    delta_dec = abs(self.DEC[i,j] - center_DEC)
+
+                    # Is the pixel in the mask?
+                    if delta_ra < 0.5*width and delta_dec < 0.5*height:
+                        mask.data[i,j] = 1.0
+                    else:
+                        mask.data[i,j] = np.nan
+        
+        print('Providing requested mask, have fun!')
+        return mask
+    
+# =============================================================================
+# Creating a mask for a region of the map
+# =============================================================================
+    def CombineMasks(self, mask1, mask2):
+        # Method to combine two masks into one for data analysis
+        
+        # Initialization
+        # mask1, mask2 : Fits files for the masks to be combined
+        
+        print()
+        print('========================')
+        print('Combining friendly masks')
+        print('========================')
+        print()
+        
+        # Size of the maps
+        size1 = np.shape(mask1.data)
+        size2 = np.shape(mask2.data)
+        size_ref = self.size
+        
+        # Creating the empty array for the final mask
+        mask = fits.PrimaryHDU(data=np.zeros(self.size), header=self.header)
+        
+        # Check that both masks have the same size as the obs object
+        if size1 != size_ref or size2 != size_ref:
+            print('Masks must have the same shape as the data to be analyzed')
+            return mask
+        
+        # Creating the combined mask
+        mask.data[np.where(mask.data < 1)] = np.nan
+        mask.data[np.where(mask1.data > 0)] = 1.0  
+        mask.data[np.where(mask2.data > 0)] = 1.0  
+
+        print('Providing combined mask, good luck!')
+        return mask
     
 # =============================================================================
 # Function to create an obs object from a HAWC+ data cube
@@ -304,7 +471,7 @@ def load_hawc(fits_name):
     print()
     
     # Unit conversion from Jy/pixel to mJy/arcsec^2
-    hawc_obs.pixel = (hawc_data[0].header['PIXSCAL']) # Pixel scale of the data in arcseconds
+    hawc_obs.pixel = 3600.0*(hawc_data[0].header['CDELT2']) # Pixel scale of the data in arcseconds
     hawc_obs.units = 'mJy per arcsec$^2$' # Setting working units
     conv = 1000.0*hawc_obs.pixel**-2.0 # Conversion factor from Jy/pixel
     
@@ -325,6 +492,15 @@ def load_hawc(fits_name):
     
     # Finding the Y and X size of the arrays 
     hawc_obs.size = hawc_obs.I.shape # hawc_obs.size should be in the form (Y,X)
+    
+    # Expanding the arrays of coordinates
+    hawc_obs.RA = np.zeros(hawc_obs.size)
+    hawc_obs.DEC = np.zeros(hawc_obs.size)
+    # Identifying the world coordinates for each pixel - NOT EFFICIENT?    
+    for i in range (0,hawc_obs.size[0]): # Declination
+        for j in range (0,hawc_obs.size[1]): # Right Ascension
+            hawc_obs.RA[i,j], hawc_obs.DEC[i,j] = (
+                hawc_obs.astrometry.array_index_to_world_values(i,j))
 
     # Closing access to the fits file
     hawc_data.close()
@@ -343,7 +519,7 @@ def load_pol2(target, fits_imap, fits_qmap, fits_umap):
     # fits_qmap : String for the name of the Stokes Q map to be loaded
     # fits_umap : String for the name of the Stokes U map to be loaded
     
-    # This function is valid as of 2020/01/23
+    # This function is valid as of 2020/05/17
            
     print()
     print('=============================')
@@ -379,7 +555,7 @@ def load_pol2(target, fits_imap, fits_qmap, fits_umap):
     print('Wavelength observed: ' + str(pol2_obs.wavelength) + ' µm')
     print()
     
-    # Unit conversion from Jy/beam to mJy/arcsec^2
+    # Unit conversion from pW/beam to Jy/beam to mJy/arcsec^2
     # Beam size in arcseconds
     if pol2_obs.wavelength == 450.0:
         pol2_obs.beam = 9.8 # Effective beam size at 450 um in arcseconds
@@ -391,14 +567,15 @@ def load_pol2(target, fits_imap, fits_qmap, fits_umap):
         print('Sorry, but no valid wavelength was provided...')
     
     # Loading data for every attribute of hawc_obs
-    pol2_obs.I = np.squeeze(conv*pol2_imap[0].data) # Stokes I
-    pol2_obs.dI = np.squeeze(conv*pol2_imap[1].data**0.5) # Uncertainty dI for Stokes I
-    pol2_obs.Q = np.squeeze(conv*pol2_qmap[0].data) # Stokes Q
-    pol2_obs.dQ = np.squeeze(conv*pol2_qmap[1].data**0.5) # Uncertainty dQ for Stokes Q
-    pol2_obs.U = np.squeeze(conv*pol2_umap[0].data) # Stokes U
-    pol2_obs.dU = np.squeeze(conv*pol2_umap[1].data**0.5) # Uncertainty dU for Stokes U
+    pol2_obs.I = np.squeeze(conv*pol2_imap[0].data[0]) # Stokes I
+    pol2_obs.dI = np.squeeze(conv*pol2_imap[1].data[0]**0.5) # Uncertainty dI for Stokes I
+    pol2_obs.Q = np.squeeze(conv*pol2_qmap[0].data[0]) # Stokes Q
+    pol2_obs.dQ = np.squeeze(conv*pol2_qmap[1].data[0]**0.5) # Uncertainty dQ for Stokes Q
+    pol2_obs.U = np.squeeze(conv*pol2_umap[0].data[0]) # Stokes U
+    pol2_obs.dU = np.squeeze(conv*pol2_umap[1].data[0]**0.5) # Uncertainty dU for Stokes U
     
     # Adapting the header if a third dimension exists in the data
+    # Ugly workaround, may need to be improved in the future
     pol2_obs.header['NAXIS'] = 2
     pol2_obs.header.remove('NAXIS3')
     pol2_obs.header.remove('LBOUND3')
@@ -417,6 +594,8 @@ def load_pol2(target, fits_imap, fits_qmap, fits_umap):
             ('BMIN',pol2_obs.beam/3600.0, 'Beam minor axis'))
     pol2_obs.header.append(('BPA',0.0, 'Beam position angle'))
     pol2_obs.astrometry = wcs.WCS(pol2_obs.header) # Astrometry information for the data
+    #pol2_obs.astrometry = pol2_obs.astrometry.dropaxis(2) # Potential fix, investigate
+    
     
     # Finding the Y and X size of the arrays 
     pol2_obs.size = pol2_obs.I.shape # pol2_obs.size should be in the form (Y,X)
@@ -447,10 +626,8 @@ def load_pol2(target, fits_imap, fits_qmap, fits_umap):
     pol2_obs.dP = pol2_obs.P*((pol2_obs.dPI/pol2_obs.PI)**2.0 + 
                               (pol2_obs.dI/pol2_obs.I)**2.0)**0.5
     
-    # Q and U ratio
-    ratioQU = pol2_obs.U/pol2_obs.Q
     # Polarization angle O
-    pol2_obs.O = (0.5*180.0/math.pi)*np.arctan(ratioQU)
+    pol2_obs.O = (0.5*180.0/math.pi)*np.arctan2(pol2_obs.U, pol2_obs.Q)
     # Uncertainties dO
     pol2_obs.dO = (0.5*180.0/math.pi)*((pol2_obs.Q*pol2_obs.dU)**2.0 + 
                        (pol2_obs.U*pol2_obs.dQ)**2.0)**0.5/pi_biased
@@ -458,6 +635,15 @@ def load_pol2(target, fits_imap, fits_qmap, fits_umap):
     pol2_obs.B = pol2_obs.O + 90.0
     pol2_obs.B[np.where(pol2_obs.B > 90.0)] = pol2_obs.B[np.where(pol2_obs.B 
                > 90.0)] - 180.0  
+    
+    # Expanding the arrays of coordinates
+    pol2_obs.RA = np.zeros(pol2_obs.size)
+    pol2_obs.DEC = np.zeros(pol2_obs.size)
+    # Identifying the world coordinates for each pixel - NOT EFFICIENT?    
+    for i in range (0,pol2_obs.size[0]): # Declination
+        for j in range (0,pol2_obs.size[1]): # Right Ascension
+            pol2_obs.RA[i,j], pol2_obs.DEC[i,j] = (
+                pol2_obs.astrometry.array_index_to_world_values(i, j))
     
     # Closing access to the fits files
     pol2_imap.close()